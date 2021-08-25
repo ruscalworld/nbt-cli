b59a22553c04cf39bef0da40a32a936be3d4f348
RuscalWorld <me@ruscalworld.ru>
2021-08-25 09:25:23 +0300

    Improve trees

--- tree.go
+++ tree.go
@@ -3,9 +3,18 @@ package main
 import (
 	"fmt"
 	"log"
+	"strings"
 
 	"github.com/disiqueira/gotree"
 	"github.com/urfave/cli/v2"
+)
+
+const (
+	newLine      = "\n"
+	emptySpace   = "    "
+	middleItem   = "├── "
+	continueItem = "│   "
+	lastItem     = "└── "
 )
 
 func PrintTree(_ *cli.Context) error {
@@ -16,7 +25,7 @@ func PrintTree(_ *cli.Context) error {
 
 	root := gotree.New(InputFilePath)
 	processNode(&root, CurrentData)
-	log.Println(root.Print())
+	log.Println(printTree(root))
 	return nil
 }
 
@@ -29,4 +38,55 @@ func processNode(parent *gotree.Tree, data map[string]interface{}) {
 			(*parent).Add(fmt.Sprintf("%s: %s", key, value))
 		}
 	}
+}
+
+func printTree(tree gotree.Tree) string {
+	return tree.Text() + newLine + printItems(tree.Items(), []bool{})
+}
+
+func printItems(tree []gotree.Tree, spaces []bool) string {
+	var result string
+	for i, f := range tree {
+		last := i == len(tree)-1
+		result += printText(f.Text(), spaces, last)
+		if len(f.Items()) > 0 {
+			spacesChild := append(spaces, last)
+			result += printItems(f.Items(), spacesChild)
+		}
+	}
+	return result
+}
+
+func printText(text string, spaces []bool, last bool) string {
+	var result string
+	for _, space := range spaces {
+		if space {
+			result += emptySpace
+		} else {
+			result += continueItem
+		}
+	}
+
+	indicator := middleItem
+	if last {
+		indicator = lastItem
+	}
+
+	var out string
+	lines := strings.Split(text, "\n")
+	for i := range lines {
+		text := lines[i]
+		if i == 0 {
+			out += result + indicator + text + newLine
+			continue
+		}
+		if last {
+			indicator = emptySpace
+		} else {
+			indicator = continueItem
+		}
+		out += result + indicator + text + newLine
+	}
+
+	return out
 }
